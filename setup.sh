7c377816381cb6e03f9853dbc01abd4d5d74303c
JungWKim <lionmini@naver.com>
2023-03-23 16:40:21 +0900

    .

---
+++ setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+IP=
+
+cd ~
+
+# disable firewall
+systemctl stop ufw
+systemctl disable ufw
+
+# prevent auto upgrade
+sudo sed -i 's/1/0/g' /etc/apt/apt.conf.d/20auto-upgrades
+
+# install basic packages
+sudo apt install -y net-tools nfs-common whois
+
+# network configuration
+sudo modprobe overlay \
+    && sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+# install containerd
+sudo apt-get update
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update \
+    && sudo apt-get install -y containerd.io
+
+sudo mkdir -p /etc/containerd \
+    && sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# ssh configuration
+ssh-keygen -t rsa
+
+ssh-copy-id -i ~/.ssh/id_rsa ${USER}@${IP}
+
+# k8s installation via kubespray
+sudo apt install -y python3-pip
+git clone -b release-2.19 https://github.com/kubernetes-sigs/kubespray.git
+cd kubespray
+pip install -r requirements.txt
+
+cp -rfp inventory/sample inventory/mycluster
+declare -a IPS=(${IP})
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+
+ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root cluster.yml -K
+
+# install nvidia-container-toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update \
+    && sudo apt-get install -y nvidia-container-toolkit
+
+
