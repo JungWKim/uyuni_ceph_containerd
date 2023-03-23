25ab29c3bfedb6d53d0dbc1e92d90c0e3b79485c
JungWKim <lionmini@naver.com>
2023-03-23 19:20:20 +0900

    .

--- setup.sh
+++ setup.sh
@@ -72,6 +72,16 @@ declare -a IPS=(${IP})
 CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
 
 ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root cluster.yml -K
+cd ~
+
+# enable kubectl in admin account and root
+mkdir -p ${HOME}/.kube
+sudo cp -i /etc/kubernetes/admin.conf ${HOME}/.kube/config
+sudo chown ${USER}:${USER} ${HOME}/.kube/config
+
+# enable kubectl & kubeadm auto-completion
+echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
+echo "source <(kubeadm completion bash)" >> ${HOME}/.bashrc
 
 # install nvidia-container-toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
@@ -81,4 +91,58 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
 sudo apt-get update \
     && sudo apt-get install -y nvidia-container-toolkit
 
+mv ~/xiilab/config.toml /etc/containerd/
+sudo systemctl restart containerd
 
+# install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# install rook ceph
+git clone https://github.com/rook/rook.git
+helm repo add rook-release https://charts.rook.io/release
+helm search repo rook-ceph
+kubectl create namespace rook-ceph
+helm install --namespace rook-ceph rook-ceph rook-release/rook-ceph
+sleep 60
+
+# enable toolbox
+sed -i "26s/false/true/g" ~/rook/deploy/charts/rook-ceph-cluster/values.yaml
+# reduce monitor daemon from 3 to 1
+sed -i "s/count: 3/count: 1/g" ~/rook/deploy/charts/rook-ceph-cluster/values.yaml
+# reduce manager daemon from 3 to 1
+sed -i "s/count: 2/count: 1/g" ~/rook/deploy/charts/rook-ceph-cluster/values.yaml
+# reduce cephBlock datapoolsize from 3 to 2
+sed -i "429s/3/2/g" ~/rook/deploy/charts/rook-ceph-cluster/values.yaml
+# reduce cephFilesystem metadata pool size from 3 to 2
+sed -i "492s/3/2/g" ~/rook/deploy/charts/rook-ceph-cluster/values.yaml
+# reduce cephFilesystem data pool size from 3 to 2
+sed -i "496s/3/2/g" ~/rook/deploy/charts/rook-ceph-cluster/values.yaml
+
+#--- install rook ceph cluster
+cd ~/rook/deploy/charts/rook-ceph-cluster
+helm install -n rook-ceph rook-ceph-cluster --set operatorNamespace=rook-ceph rook-release/rook-ceph-cluster -f values.yaml
+cd ~
+sleep 60
+
+# install helmfile
+wget https://github.com/helmfile/helmfile/releases/download/v0.150.0/helmfile_0.150.0_linux_amd64.tar.gz
+tar -zxvf helmfile_0.150.0_linux_amd64.tar.gz
+sudo mv helmfile /usr/bin/
+rm LICENSE && rm README.md && rm helmfile_0.150.0_linux_amd64.tar.gz
+
+# install uyuni infra
+git clone https://github.com/xiilab/Uyuni_Deploy.git
+cd ~/Uyuni_Deploy
+helmfile --environment test -l type=base sync
+cd ~
+
+# set ceph-filesystem as default storageclass
+kubectl patch storageclass nfs-client -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass ceph-bucket -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass ceph-filesystem -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+# install uyuni suite
+git clone https://github.com/xiilab/Uyuni_Kustomize.git
